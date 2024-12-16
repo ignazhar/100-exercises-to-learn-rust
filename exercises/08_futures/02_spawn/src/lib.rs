0d9663b98504b8fb5b8db5c1f2a6ecabb89f0a7d
Ihnat Zharikhin <ignazhar@gmail.com>
2024-12-16 11:32:50 +0200

    08 chapter 02 exercise first implementation

--- exercises/08_futures/02_spawn/src/lib.rs
+++ exercises/08_futures/02_spawn/src/lib.rs
@@ -1,10 +1,28 @@
+use std::sync::mpsc::sync_channel;
+
 use tokio::net::TcpListener;
 
 // TODO: write an echo server that accepts TCP connections on two listeners, concurrently.
 //  Multiple connections (on the same listeners) should be processed concurrently.
 //  The received data should be echoed back to the client.
 pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    loop {
+        // first
+        let (mut socket, _) = first.accept().await?;
+
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = socket.split();
+            tokio::io::copy(&mut reader, &mut writer).await;
+        });
+
+        // second
+        let (mut socket, _) = second.accept().await?;
+
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = socket.split();
+            tokio::io::copy(&mut reader, &mut writer).await;
+        });
+    }
 }
 
 #[cfg(test)]
